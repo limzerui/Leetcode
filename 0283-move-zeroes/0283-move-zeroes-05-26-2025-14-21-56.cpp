002a4e9a7442e1b25168210e05d3a4324d7600eb
Zerui <106159827+limzerui@users.noreply.github.com>
2025-05-26 14:21:56 +0800

    Time: 0 ms (100%), Space: 23.8 MB (49.71%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes-05-26-2025-14-21-56.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastnonzeroindex = 0;
+        for(int i = 0; i< nums.size(); i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[lastnonzeroindex] = nums[i];
+                lastnonzeroindex++;
+            }
+        }
+        for(int i = lastnonzeroindex ; i<nums.size(); i++)
+        {
+            nums[i] = 0;
+        }
+        
+    }
+};
