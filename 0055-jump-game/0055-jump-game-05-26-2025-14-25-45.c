914df2141f2ea4c25d3657d59b73cb9a3520fcd5
Zerui <106159827+limzerui@users.noreply.github.com>
2025-05-26 14:25:46 +0800

    Time: 4 ms (16.71%), Space: 10.7 MB (19.87%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game-05-26-2025-14-25-45.c
@@ -0,0 +1,16 @@
+bool canJump(int* nums, int numsSize) {
+    int farthest = 0;
+
+    for(int i = 0; i<numsSize; i++)
+    {
+        if(i > farthest)
+        {
+            return false;
+        }
+        if(i+nums[i] > farthest)
+        {
+            farthest = i + nums[i];
+        }
+    }
+    return true;
+}
