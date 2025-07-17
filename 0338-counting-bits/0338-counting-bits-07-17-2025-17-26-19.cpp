9e63c98a57f0e9263d563090c415f64c6b1a6321
Zerui <106159827+limzerui@users.noreply.github.com>
2025-07-17 17:26:20 +0800

    Time: 0 ms (100%), Space: 10.7 MB (58.23%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits-07-17-2025-17-26-19.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        //create a n+1 array
+        vector<int> bits(n+1, 0);
+
+        for(int i = 1; i<=n; i++)
+        {
+            bits[i] = bits[i>>1] + (i&1);
+
+        }
+        return bits;
+    }
+};
